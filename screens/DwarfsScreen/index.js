f68518c93f00a4a4b20cf02c03e09a831532c009
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-02-10 16:04:22 +0100

    Fetch dwarfs and show the gallery

--- screens/DwarfsScreen/index.js
+++ screens/DwarfsScreen/index.js
@@ -1,12 +1,73 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  StyleSheet,
+  ScrollView,
+  Image,
+  TouchableOpacity
+} from 'react-native'
+import { listDwarfs } from '../../redux/actions/dwarfActions'
+
+import { colors } from '../../theme/colors'
 
 const DwarfsScreen = () => {
+  const dispatch = useDispatch()
+
+  const { dwarfs, loading, error } = useSelector((state) => state.dwarfList)
+
+  useEffect(() => {
+    dispatch(listDwarfs())
+  }, [dispatch])
+
   return (
-    <View>
-      <Text>Dwarfs List</Text>
+    <View style={styles.container}>
+      {loading ? (
+        <View>
+          <ActivityIndicator size='large' color={colors.primary} />
+        </View>
+      ) : error ? (
+        <View>
+          <Text>{error}</Text>
+        </View>
+      ) : (
+        dwarfs &&
+        dwarfs.length > 0 && (
+          <ScrollView contentContainerStyle={styles.dwarfContainer}>
+            {dwarfs.map((dwarf) => (
+              <TouchableOpacity key={dwarf.name} style={styles.imageWrapper}>
+                <Image
+                  style={styles.image}
+                  source={{ uri: dwarf.picture_src }}
+                />
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        )
+      )}
     </View>
   )
 }
 
 export default DwarfsScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  dwarfContainer: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  imageWrapper: {
+    margin: 10
+  },
+  image: {
+    width: 180,
+    height: 180,
+    borderRadius: 10
+  }
+})
