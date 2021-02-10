a4b620f42d45468d6a2831fa6e3945a6b2271ac8
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-02-10 16:05:27 +0100

    Center ActivityIndicator

--- screens/DwarfsScreen/index.js
+++ screens/DwarfsScreen/index.js
@@ -55,7 +55,9 @@ export default DwarfsScreen
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   dwarfContainer: {
     justifyContent: 'space-between',
