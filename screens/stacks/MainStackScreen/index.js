a6fa20cc1414165a3ed49db2ef5409e7b1732c48
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-02-11 21:28:26 +0100

    Add CustomAppbar to the MainStack, add MapScreen

--- screens/stacks/MainStackScreen/index.js
+++ screens/stacks/MainStackScreen/index.js
@@ -1,35 +1,21 @@
 import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
-import MenuScreen from '../../MenuScreen'
+import MapScreen from '../../MapScreen'
 import DwarfsScreen from '../../DwarfsScreen'
 import DwarfDetailsScreen from '../../DwarfDetailsScreen'
+import CustomAppbar from '../../../components/UI/CustomAppbar'
 
 const MainStack = createStackNavigator()
 
 const MainStackScreen = () => {
   return (
-    <MainStack.Navigator initialRouteName='Menu'>
-      <MainStack.Screen
-        name='Menu'
-        component={MenuScreen}
-        options={{
-          headerShown: false
-        }}
-      />
-      <MainStack.Screen
-        name='Dwarfs'
-        component={DwarfsScreen}
-        options={{
-          headerShown: false
-        }}
-      />
-      <MainStack.Screen
-        name='DwarfDetails'
-        component={DwarfDetailsScreen}
-        options={{
-          headerShown: false
-        }}
-      />
+    <MainStack.Navigator
+      screenOptions={{ header: (props) => <CustomAppbar {...props} /> }}
+      initialRouteName='Map'
+    >
+      <MainStack.Screen name='Map' component={MapScreen} />
+      <MainStack.Screen name='Dwarfs' component={DwarfsScreen} />
+      <MainStack.Screen name='DwarfDetails' component={DwarfDetailsScreen} />
     </MainStack.Navigator>
   )
 }
